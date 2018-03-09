8e48b26edd4974519e1bd77f88df4d7a92f5de75
Harlan T Wood <code+harlan@harlantwood.net>
2018-03-09 02:04:42 -1000

    Fix test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhoenixAndElm.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Crafted with ♥"
   end
 end
