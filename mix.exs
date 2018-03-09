e0b8fb9454f206d14c799efa4a28f6fe0fc959ed
Harlan T Wood <code+harlan@harlantwood.net>
2018-03-09 02:23:56 -1000

    Test coverage + Travis + Coveralls

--- mix.exs
+++ mix.exs
@@ -11,7 +11,8 @@ defmodule PhoenixAndElm.Mixfile do
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: Coverex.Task, coveralls: true, ignore_modules: []]
     ]
   end
 
@@ -45,15 +46,16 @@ defmodule PhoenixAndElm.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.1"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
+      {:coverex, "~> 1.4.10", only: :test},
       {:cowboy, "~> 1.0"},
       {:faker, git: "https://github.com/bigardone/faker", branch: "master"},
+      {:gettext, "~> 0.11"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
       {:scrivener_ecto, "~> 1.0"}
     ]
   end
