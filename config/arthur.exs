d505796b1fea58f34b4a69f342b06dc4c885c638
Harlan T Wood <code+harlan@harlantwood.net>
2018-03-11 02:09:38 -1000

    ...

--- config/arthur.exs
+++ config/arthur.exs
@@ -2,16 +2,15 @@ use Mix.Config
 
 config :arthur, :push, [
   {:post,
-    [
-      "#{Path.expand("node_modules/.bin/elm-format", System.cwd!())} web/elm/src --yes"
-    ]}
+   [
+     "#{Path.expand("node_modules/.bin/elm-format", System.cwd!())} web/elm/src --yes"
+   ]}
 ]
 
 config :arthur, :ci, [
   {:post,
-    [
-      "mix run priv/repo/seeds.exs > /dev/null",
-      "#{Path.expand("node_modules/.bin/elm-format", System.cwd!())} --validate web/elm/src"
-    ]}
+   [
+     "mix run priv/repo/seeds.exs > /dev/null",
+     "#{Path.expand("node_modules/.bin/elm-format", System.cwd!())} --validate web/elm/src"
+   ]}
 ]
-
